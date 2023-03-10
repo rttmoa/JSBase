2d2a4ace3df85061f6a0a6575872170a91527888
rttmoa <rttmoa@gmail.com>
2023-03-10 22:37:46 +0800

    递归求斐波那契数列

--- 递归函数.js
+++ 递归函数.js
@@ -16,3 +16,16 @@ const rearr = sort(arr, ['id', 'age'])
 // console.log(rearr)
 
 
+// 递归求斐波那契数列
+    // 斐波那契数列指的是这样一个数列：
+    //     1 1 2 3 5 8 13 21 34 55 …
+    //     这个数列从第3项开始，每一项都等于前两项之和
+function sequence (n) { // n 就是要求的第n位
+    // 设置终点
+    if(n === 1 || n === 2){
+        return 1;
+    }
+    // 未到终点
+    return sequence(n - 1) + sequence(n - 2)
+}
+// sequence(5)
